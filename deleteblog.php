fb0122ee580a591f21133da3fc7de779cc84a676
NourHajji <Nour.Hajji@esprit.tn>
2024-12-08 01:18:59 +0100

    nour

--- deleteblog.php
+++
@@ -1,6 +0,0 @@
-<?php 
-include "../../controller/blogc.php";
-$b = new BlogC();
-$b->deleteBlog($_GET['id']);
-header("location:dashboard.php");
-?>
--- deletecom.php
+++
@@ -1,6 +0,0 @@
-<?php 
-include "../../controller/comentairec.php";
-$b = new comentaireC();
-$b->deletecomentaire($_GET['id_c']);
-header("location:dashboardcom.php");
-?>
